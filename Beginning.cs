4d9beece33af99432a1617278ffcefe0dbc69f91
Neonebu <oguzcansirolu1@gmail.com>
2022-01-13 22:24:59 +0300

    FIRST STEP ONLY ONE THAT MATTERS

---
+++ Beginning.cs
@@ -0,0 +1,42 @@
+﻿// Oğuzcan "Neonebu" Şirolu
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace NeonebuMain
+{
+    public class Beginning
+    {
+        public void MaximizeFromSystemTray()
+        {
+        }
+        public void MinimizeToSystemTray()
+        {
+
+        }
+        public void RegisterInStartup()
+        {
+            try
+            {
+                RegistryKey registryKey = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+                if (registryKey.GetValue("NeonebuWindows") == null)
+                {
+                    registryKey.SetValue("NeonebuWindows", System.Reflection.Assembly.GetExecutingAssembly().Location);
+                }
+                else
+                {
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// Oğuzcan "Neonebu" Şirolu
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NeonebuMain
+{
+    static class Program
+    {
+        static Form1 form1;
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            form1 = new Form1();
+            Beginning beginning = new Beginning();
+            beginning.RegisterInStartup();
+            Application.Run(form1);
+        }
+    }
+}
